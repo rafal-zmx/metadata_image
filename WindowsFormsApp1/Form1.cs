2270d5a3597ea3149842c9536313270a7ad47cf0
rafal-zmx <55846450+rafal-zmx@users.noreply.github.com>
2022-06-02 12:25:29 +0200

    dodanie programu

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//przycisk wczytaj zdjęcie
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//przyzcisk zamknij
+        {
+            this.Close();
+        }
+    }
+}
