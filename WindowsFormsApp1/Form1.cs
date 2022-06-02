b0e8ff56456382456c34a546854c2bffe7c6883f
rafal-zmx <55846450+rafal-zmx@users.noreply.github.com>
2022-06-02 12:32:21 +0200

    dodanie kodu
    Dodanie metody otwierania plików, walidacja (try-catch)

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,7 +13,17 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)//przycisk wczytaj zdjęcie
         {
+            openFileDialog1.Title = "Otwórz";//tytuł okna #1
+            openFileDialog1.Filter = "bmp (*.bmp)|*.bmp| jpg (*.jpg)|*.jpg";//format pliku do odczytu #2
 
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)//przyzcisk zamknij
